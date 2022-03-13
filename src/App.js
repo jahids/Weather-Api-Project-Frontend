1efef3a1c2cc99c861eb0701ef3f7b345e969f9e
jahids <evanjahid321@gmail.com>
2022-03-13 09:52:59 +0600

    'nammming + simple bug fix'

--- src/App.js
+++ src/App.js
@@ -7,73 +7,77 @@ import { click } from '@testing-library/user-event/dist/click';
 
 
 function App() {
-  const [user, setUser] = useState({});
-  const [display, setdisplay] = useState([]);
+  const [Countries, setCountries] = useState({});
+  const [Users, setUsers] = useState([]);
   const [Name, setname] = useState();
 
 
 
-  const nameRef = useRef();
- 
+  const getInputValue = useRef();
 
-  const handleTargetname = (usar) => {
-    nameRef.current.value = usar;
-    
-    console.log('test tagert', usar);
 
-    setname(usar)
+
+
+
+  const handleCatchInputValue = (user) => {
+    getInputValue.current.value = user;
+
+    console.log('test tagert', user);
+
+    setname(user)
     processChange()
-    // setdisplay('')
-   
+    setUsers('')
+
 
 
 
   }
 
 
-  const handlePassName = () => {
+  const handlePassInputName = () => {
+
+    // if(nameRef.current.value === ''){
+    //   var name = 'bangladesh'
+    //   console.log('handle pass name chk', name);
+    //   const namejson = { name };
+
+    //   // console.log(name);
+    //   fetch(`http://localhost:3007/country`, {
+    //     method: 'post',
+    //     headers: { 'content-type': 'application/json' },
+    //     body: JSON.stringify(namejson)
+    //   })
+    //     .then((res) => res.json())
+
+    //     .then((data) => setUser(data));
+
+
+    //   console.log(user);
+
+    //   // processChange()
+
+    // }
+    // else{
+
+    let name = getInputValue.current.value;
+    console.log('handle pass name chk', name);
+    const NameConvertObject = { name };
+
+    // console.log(name);
+    fetch(`http://localhost:3007/country`, {
+      method: 'post',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(NameConvertObject)
+    })
+      .then((res) => res.json())
+
+      .then((data) => setCountries(data));
+
+
+    console.log(Countries);
+
    
-if(nameRef.current.value === ''){
-  var name = 'bangladesh'
-  console.log('handle pass name chk', name);
-  const namejson = { name };
 
-  // console.log(name);
-  fetch(`http://localhost:3007/country`, {
-    method: 'post',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify(namejson)
-  })
-    .then((res) => res.json())
-
-    .then((data) => setUser(data));
-
-
-  console.log(user);
- 
-  // processChange()
-
-}else{
-
-  name = nameRef.current.value;
-  console.log('handle pass name chk', name);
-  const namejson = { name };
-
-  // console.log(name);
-  fetch(`http://localhost:3007/country`, {
-    method: 'post',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify(namejson)
-  })
-    .then((res) => res.json())
-
-    .then((data) => setUser(data));
-
-
-  console.log(user);
-
-}
-   
 
   }
 
@@ -90,7 +94,7 @@ if(nameRef.current.value === ''){
   }
   // using debounce
 
-  const processChange = debounce(() => handlePassName());
+  const processChange = debounce(() => handlePassInputName());
 
   // name suggestion
 
@@ -98,41 +102,62 @@ if(nameRef.current.value === ''){
   const matchPeople = (input) => {
 
     if (input === "") {
-              console.log('no value click');
-    }else{
+      console.log('no value click');
+    } else {
 
       let reg = new RegExp(input, 'i')
 
       return browserList.map(element => {
-  
+
         if (element.match(reg)) {
-  
+
           return element
         }
       });
 
     }
 
-   
+
 
   }
 
   const changeInput = () => {
-
+    let name = getInputValue.current.value;
     // const name = nameRef.current.value;
-    const name = nameRef.current.value;
+    if (name === '') {
+      name = 'a'
+      matchPeople(name);
+    }
 
     const autoCompleteResult = matchPeople(name);
     console.log(autoCompleteResult);
-    setdisplay(autoCompleteResult)
+    setUsers(autoCompleteResult)
   }
 
 
 
 
-  const handleroot = () =>{
-setdisplay([])
+  const handleroot = (event) => {
+    console.log('handle root');
+
+    //  setdisplay([]);
+    // event.stopPropagation();
+    // window.onload=function(){
+    //   var hidediv = document.getElementById('div1');
+    //   document.onclick = function(div){
+    //     if(div.target.id !== 'div1'){
+    //       hidediv.style.display='none';
+    //       console.log('thsi raw js touch');
+    //     }
+    //   }
+    // }
+
+
+
   }
+
+
+
 
 
 
@@ -143,17 +168,18 @@ setdisplay([])
 
 
   return (
-    <header onClick={()=>{handleroot()}} id='head'>
+    <header onClick={() => { handleroot() }} id='head'>
       <h1 className='head' >Weather in </h1>
       <form id='blg' className="search-loaction searchbox">
         <input
-          ref={nameRef}
+          ref={getInputValue}
+          autoComplete='off'
           // list="browsers"
           // id="browsers"
           id="textinput"
           type="text"
           name="city"
-          onChange={changeInput}
+          onSelect={changeInput}
           placeholder="What city?"
           onKeyUp={() => { processChange() }}
           // onChange={() => { handlePassName() }}
@@ -161,22 +187,17 @@ setdisplay([])
 
         />
 
-        <ul className="suggestions">
+        <ul className="suggestions ">
 
-         
+
           {
-
-             
-
-            display && display.length > 0 && display.map(mapuser => <li
+            Users && Users.length > 0 && Users.map(User => <li
+              id='div1'
               onClick={() => {
-                //
-                handleTargetname(mapuser)
+                handleCatchInputValue(User)
               }}
-
             >
-
-              {mapuser}
+              {User}
 
             </li>)
           }
@@ -187,7 +208,7 @@ setdisplay([])
       <div className="card rounded my-3 shadow-lg back-card">
         <div className="card-top text-center my-3">
           <div className="city-name my-3">
-            <p>{user?.location?.name || 'chittagong'}</p>
+            <p>{Countries?.location?.name || 'chittagong'}</p>
             <span>...</span>
           </div>
         </div>
@@ -195,26 +216,26 @@ setdisplay([])
         <div className="card-body my-5">
           <div className="card-mid row">
             <div className="col-8 text-center temp">
-              <span>{user?.current?.temp_c|| '36,9'}&deg;C</span>
+              <span>{Countries?.current?.temp_c || '36,9'}&deg;C</span>
             </div>
             <div className="col-4 condition-temp">
               {/* <p className="condition">{this.props.weatherResult.weather[0].description}</p> */}
-              <p className="high">tz_id: {user?.location?.tz_id || 'Asia/Dhaka'}&deg;C</p>
-              <h2 className="low">Country {user?.location?.country|| "Bangladesh"}</h2>
+              <p className="high">tz_id: {Countries?.location?.tz_id || 'Asia/Dhaka'}&deg;C</p>
+              <h2 className="low">Country {Countries?.location?.country || "Bangladesh"}</h2>
             </div>
           </div>
 
           <div className="icon-container card shadow mx-auto">
-            <img src={user?.current?.condition?.icon || 'http://cdn.weatherapi.com/weather/64x64/day/113.png'} alt="" srcset="" />
+            <img src={Countries?.current?.condition?.icon || 'http://cdn.weatherapi.com/weather/64x64/day/113.png'} alt="" srcset="" />
 
           </div>
           <div className="card-bottom px-5 py-4 row">
             <div className="col text-center">
-              <p>{user?.current?.feelslike_c || "35.6"}&deg;C</p>
+              <p>{Countries?.current?.feelslike_c || "35.6"}&deg;C</p>
               <span>Feels Like</span>
             </div>
             <div className="col text-center">
-              <p>{user?.current?.humidity || '15'}%</p>
+              <p>{Countries?.current?.humidity || '15'}%</p>
               <span>Humidity</span>
             </div>
           </div>
